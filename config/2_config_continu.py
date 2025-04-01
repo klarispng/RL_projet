63b7632ee1f187d71e029fdf2642ddda48e60153
klaris.png <clarisse.debray@gmail.com>
2025-04-01 10:09:52 +0200

    init

---
+++ config/2_config_continu.py
@@ -0,0 +1,37 @@
+import pickle
+
+config_dict = {
+    "observation": {
+        "type": "Kinematics",
+        "vehicles_count": 5,
+        "features": ["x", "y", "vx", "vy", "cos_h", "sin_h"],
+        "absolute": False,
+        "normalize": True,
+    },
+    "action": {
+        "type": "ContinuousAction",
+    },
+    "lanes_count": 4,
+    "vehicles_count": 10,
+    "duration": 60,  # [s]
+    "initial_spacing": 1.0,
+    "collision_reward": -1.0,
+    "right_lane_reward": 0.5,
+    "high_speed_reward": 0.1,
+    "lane_change_reward": 0,
+    "reward_speed_range": [20, 30],
+    "simulation_frequency": 15,  # [Hz]
+    "policy_frequency": 5,  # [Hz]
+    "other_vehicles_type": "highway_env.vehicle.behavior.IDMVehicle",
+    "screen_width": 600,
+    "screen_height": 150,
+    "centering_position": [0.3, 0.5],
+    "scaling": 5.5,
+    "show_trajectories": True,
+    "render_agent": True,
+    "offscreen_rendering": False,
+    "disable_collision_checks": False,
+}
+
+with open("config_continu.pkl", "wb") as f:
+    pickle.dump(config_dict, f)
