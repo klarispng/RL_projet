b96bd7fcb2746e07bff438219d5d3a90e1994bc2
arthurvogels <vogels.arthur@gmail.com>
2025-04-09 23:01:13 +0200

    Add: class for dqn

--- config/1_config_base.py
+++ config/1_config_base.py
@@ -4,18 +4,17 @@ import highway_env
 
 config_dict = {
     "observation": {
-        "type": "OccupancyGrid",
+        "type": "Kinematics",
         "vehicles_count": 10,
         "features": ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"],
         "features_range": {
             "x": [-100, 100],
             "y": [-100, 100],
             "vx": [-20, 20],
-            "vy": [-20, 20],
+            "vy": [-20, 20]
         },
-        "grid_size": [[-20, 20], [-20, 20]],
-        "grid_step": [5, 5],
         "absolute": False,
+        "order": "sorted"
     },
     "action": {
         "type": "DiscreteMetaAction",
